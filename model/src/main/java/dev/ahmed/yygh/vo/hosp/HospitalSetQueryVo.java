8aa974a87c5fda57475caeb5898e620080eb6b64
Ahmed Bughra <ahmedbughra@gmail.com>
2022-04-09 17:31:07 +0300

    conditional query and paging video 19

--- model/src/main/java/dev/ahmed/yygh/vo/hosp/HospitalSetQueryVo.java
+++ model/src/main/java/dev/ahmed/yygh/vo/hosp/HospitalSetQueryVo.java
@@ -7,9 +7,9 @@ import lombok.Data;
 @Data
 public class HospitalSetQueryVo {
 
-    @ApiModelProperty(value = "医院名称")
+    @ApiModelProperty(value = "hospital name")
     private String hosname;
 
-    @ApiModelProperty(value = "医院编号")
+    @ApiModelProperty(value = "hospital code")
     private String hoscode;
 }
--- service/service_hospital/src/main/java/dev/ahmed/yygh/hosp/controller/HospitalSetServiceController.java
+++ service/service_hospital/src/main/java/dev/ahmed/yygh/hosp/controller/HospitalSetServiceController.java
@@ -1,12 +1,17 @@
 package dev.ahmed.yygh.hosp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import dev.ahmed.yygh.common.result.Result;
 import dev.ahmed.yygh.hosp.service.HospitalSetService;
 import dev.ahmed.yygh.model.hosp.HospitalSet;
+import dev.ahmed.yygh.vo.hosp.HospitalSetQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,11 +37,7 @@ public class HospitalSetServiceController {
         List<HospitalSet> list = hospitalSetService.list();
         return Result.ok(list);
     }
-
-    // 2. find hospitalSet by id
-    // 3. add hospitalSet
-    // 4. update hospitalSet
-    // 5. delete hospitalSet
+    // 2. delet hospitalSet
     @ApiOperation(value = "Logical delete hospitalSet by id")
     @DeleteMapping("{id}")
     public Result deleteHospitalSet(@PathVariable Long id) {
@@ -47,4 +48,36 @@ public class HospitalSetServiceController {
             return Result.fail();
         }
     }
+
+    // 3. find hospital with conditional query and paging
+    @ApiOperation(value = "find hospitalSet with paging")
+    @PostMapping("findPage/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable Long current,
+                                  @PathVariable Long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        // page object, current, limit
+        Page<HospitalSet> page = new Page<>(current, limit);
+        // query wrapper
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        // get hospital name and hospital code
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        // if hospital name and hospital code are not null then add query wrapper
+        if (!StringUtils.isEmpty(hosname)) {
+            queryWrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hoscode)) {
+            queryWrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());
+        }
+        // find hospitalSet with paging
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, queryWrapper);
+
+        return Result.ok(pageHospitalSet);
+
+    }
+    // 4. add hospitalSet
+    // 5. update hospitalSet by id
+    // 6. delete hospitalSet
+    // 7. multiple delete hospitalSet
+
 }
