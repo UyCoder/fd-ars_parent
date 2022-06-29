189b77dfef0aba9e72750ec95002bbd6fa32fee4
uycoder <Thunder1987>
2022-06-30 00:38:52 +0300

    finished video 64. worked perfectly

--- service/service_cmn/src/main/java/dev/ahmed/yygh/cmn/service/impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/dev/ahmed/yygh/cmn/service/impl/DictServiceImpl.java
@@ -12,6 +12,8 @@ import dev.ahmed.yygh.model.hosp.HospitalSet;
 import dev.ahmed.yygh.vo.cmn.DictEeVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,6 +34,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
 
 
     @Override
+    @Cacheable(value = "dict", keyGenerator = "keyGenerator")
     public List<Dict> findChildData(Long id) {
         QueryWrapper<Dict> wrapper = new QueryWrapper<>();
         wrapper.eq("parent_id", id);
@@ -77,6 +80,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
 
     // import dict data
     @Override
+    @CacheEvict(value = "dict", allEntries = true)
     public void importDictData(MultipartFile file) {
         try {
             EasyExcel.read(file.getInputStream(), DictEeVo.class, new DictListener(baseMapper)).sheet().doRead();
