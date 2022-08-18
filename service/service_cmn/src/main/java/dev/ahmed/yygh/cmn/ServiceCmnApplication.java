5bc30a5c427bd9c7030f2d4d5f00bdb65f19cfe3
uycoder <Thunder1987>
2022-08-19 00:30:17 +0300

    finished video 82. worked perfectly :)

--- service/service_cmn/src/main/java/dev/ahmed/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/dev/ahmed/yygh/cmn/ServiceCmnApplication.java
@@ -2,10 +2,12 @@ package dev.ahmed.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"dev.ahmed"})
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
--- service/service_hospital/src/main/java/dev/ahmed/yygh/hosp/ServiceHospApplication.java
+++ service/service_hospital/src/main/java/dev/ahmed/yygh/hosp/ServiceHospApplication.java
@@ -3,6 +3,7 @@ package dev.ahmed.yygh.hosp;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "dev.ahmed")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
